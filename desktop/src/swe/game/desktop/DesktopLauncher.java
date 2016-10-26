73433eee38326c688c6f6c1f3c3da83acfc7bb50
denn-s <dennis.sidin942@gmail.com>
2016-10-26 18:21:44 +0200

    initial commit

---
+++ desktop/src/swe/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package swe.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import swe.game.Pong;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Pong(), config);
+	}
+}
