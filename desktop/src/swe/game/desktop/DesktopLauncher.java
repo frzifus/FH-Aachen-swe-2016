6dea9d1bedece6f6a0a7ad205df386470b6289c1
Roo <benne.freeworldpipi@googlemail.com>
2016-11-23 13:01:25 +0100

    Nur formatiert

--- desktop/src/swe/game/desktop/DesktopLauncher.java
+++ desktop/src/swe/game/desktop/DesktopLauncher.java
@@ -7,22 +7,21 @@ import com.google.gson.JsonObject;
 import swe.game.Pong;
 
 public class DesktopLauncher {
-    public static void main(String[] arg) {
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        new LwjglApplication(new Pong(), config);
+  public static void main(String[] arg) {
+    LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+    new LwjglApplication(new Pong(), config);
 
-        // TODO: Demonstration der Kommunikation mit der Hauptanwendung. Code muss an der richtigen Stelle ausgeführt werden
-        Gson gson = new Gson();
-        JsonObject jso = new JsonObject();
-        // in dem arg[] sind die namen der spieler enthalten
-        if (arg.length >= 2) {
-            jso.addProperty("winner", arg[0]);
-            jso.addProperty("loser", arg[1]);
-            String json = gson.toJson(jso);
-            System.out.println(json);
-        }
-        // TODO: Handhabung falls keine Spielernamen übergeben
-
-
+    // TODO: Demonstration der Kommunikation mit der Hauptanwendung. Code muss
+    // an der richtigen Stelle ausgeführt werden
+    Gson gson = new Gson();
+    JsonObject jso = new JsonObject();
+    // in dem arg[] sind die namen der spieler enthalten
+    if (arg.length >= 2) {
+      jso.addProperty("winner", arg[0]);
+      jso.addProperty("loser", arg[1]);
+      String json = gson.toJson(jso);
+      System.out.println(json);
     }
+
+  }
 }
