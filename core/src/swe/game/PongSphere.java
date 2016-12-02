14b96a6d4105613cd96252563e9226959370b7c1
Frzifus <benne@klimlive.de>
2016-12-02 03:25:36 +0100

    add Sphere without everything

---
+++ core/src/swe/game/PongSphere.java
@@ -0,0 +1,20 @@
+package swe.game;
+
+/**
+ * @author Frzifus
+ */
+
+public class PongSphere {
+  /**
+   * x position...
+   */
+  private int positionX;
+
+  /**
+   * y position...
+   */
+  private int positionY;
+
+  PongSphere() {}
+
+}
