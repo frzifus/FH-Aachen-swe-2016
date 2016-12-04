b71ce545fdd0fba5ea1032365bce63e9e5eec242
Frzifus <benne@klimlive.de>
2016-12-04 03:30:56 +0100

    COllision

---
+++ core/src/swe/game/Collision.java
@@ -0,0 +1,20 @@
+package swe.game;
+
+import com.badlogic.gdx.math.Intersector;
+
+/**
+ * @author Frzifus
+ */
+
+public class Collision {
+  // test
+  // void stuff() {
+  //   if(Intersector.overlaps(pongPlayerOne.getpSprite().getBoundingRectangle(),
+  //                           pongSphere.getpSprite().getBoundingRectangle()) ||
+  //      Intersector.overlaps(pongPlayerTwo.getpSprite().getBoundingRectangle(),
+  //                           pongSphere.getpSprite().getBoundingRectangle())
+  //      ) {
+  //     // DoSomething
+  //    }
+  // }
+}
